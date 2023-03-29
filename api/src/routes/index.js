86bcbb38cd7dec75698674597826e6cb495008dd
RODO.L <wuaicot8@gmail.com>
2023-03-29 09:27:30 -0300

    pushpush

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,12 +3,10 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 
 
-
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
- 
 
 
 module.exports = router;
