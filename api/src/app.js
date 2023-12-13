27350026ff4c2060bf0ca7c24583072d4f9b8f5e
RODO.L <wuaicot8@gmail.com>
2023-12-13 19:36:38 -0300

    enviroment develop is't Ok

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
       //http://localhost:3000
       //https://pi-videogames-main-mi-pi.vercel.app/
-  res.header('Access-Control-Allow-Origin', 'https://pi-videogames-main-mi-pi.vercel.app/'); 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
