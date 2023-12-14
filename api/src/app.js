118bb576ba9f459c95e85a910eda696fec5ef651
RODO.L <wuaicot8@gmail.com>
2023-12-13 22:06:09 -0300

    C:/Program Files/Git/

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(morgan('dev'));
 server.use(cors());
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-videogames-main-mi-pi.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://pi-videogames-main-mi-pi.vercel.app/');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
