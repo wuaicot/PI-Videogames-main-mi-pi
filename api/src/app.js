7c6849e859d5d7403cb6ab1db8a06b486f509715
RODO.L <wuaicot8@gmail.com>
2023-12-13 21:13:50 -0300

    modifing cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const cors = require('cors'); // Agregado
 const routes = require('./routes/index.js');
 
 require('./db.js');
@@ -11,23 +11,26 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors());  // <- Mueve el middleware CORS aquí
+server.options('*', cors());  // También aquí
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 
-// Reemplaza el bloque de código CORS manual con el middleware cors
-server.use(cors({
-  origin: 'https://pi-videogames-main-mi-pi.vercel.app',
-  credentials: true,
-  methods: 'GET, POST, OPTIONS, PUT, DELETE',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-}));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://pi-videogames-main-mi-pi.vercel.app'); 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => {
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
@@ -35,7 +38,6 @@ server.use((err, req, res, next) => {
 });
 
 module.exports = server;
-
 
 
 //http://localhost:3000
